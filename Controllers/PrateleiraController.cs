977dc3d01f899f2cfc7dc315b0df09d99f3ef6ce
William Hartung <hartung.will@gmail.com>
2020-12-08 20:36:41 -0300

    [kickoff]

---
+++ Controllers/PrateleiraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fabric.Controllers
+{
+    public class PrateleiraController : Controller
+    {
+        // GET: Prateleira
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
